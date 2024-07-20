067d5f292c8f45cefb6833ac847c65569f3ae2d9
Sabrine-Skhiri <sabrineskhiri22@gmail.com>
2024-07-20 05:45:00 -0500

    modifications

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express= require ("express")
-//create instance
-const app= express();
-const PORT = 2020;
-//create server
-app.listen(PORT, (error) =>{
-    error ? console.log(error) : console.log("server is running on port $(PORT)");
-}) ;
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+// Création du serveur
+app.listen(PORT, (error) => {
+  error ? console.log(error) : console.log(`Le serveur écoute sur le port ${PORT}`);
+});
